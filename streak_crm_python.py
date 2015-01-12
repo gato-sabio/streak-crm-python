84ff6514077e87a3a0afe89b1fba3bf4745ffb5c
andy7panda <panda.andy@gmail.com>
2015-01-11 22:45:23 -0300

    + pipeline get, create, delete methods

--- streak_crm_python.py
+++ streak_crm_python.py
@@ -27,7 +27,7 @@ def create_attrs(attr_dict, obj):
 
 
 def api_get(self, request_path):
-    return self.streak_connection.read_api_data(request_path)
+    return self.streak_connection.get_api_data(request_path)
 
 
 class StreakConnection:
@@ -36,14 +36,38 @@ class StreakConnection:
         self.user = self.User(self)
         self.pipeline = self.Pipeline(self)
 
-    def read_api_data(self, api_path: str):
+    def get_api_data(self, api_path: str):
         api_full_path = self.settings.api_endpoint + api_path
-        print('[API]', api_full_path)
+        print('[API GET]', api_full_path)
         try:
             result = requests.get(api_full_path, auth=HTTPBasicAuth(self.settings.api_key, ''))
         except requests.HTTPError:
-            print('[HTTP] Error')
-            result = None
+            print('[HTTP GET] Error')
+            exit()
+        else:
+            result = json.loads(result.text)
+        return result
+
+    def put_api_data(self, api_path: str, data_dict):
+        api_full_path = self.settings.api_endpoint + api_path
+        print('[API PUT]', api_full_path)
+        try:
+            result = requests.put(api_full_path, data=data_dict, auth=HTTPBasicAuth(self.settings.api_key, ''))
+        except requests.HTTPError:
+            print('[HTTP PUT] Error')
+            exit()
+        else:
+            result = json.loads(result.text)
+        return result
+
+    def delete_api_data(self, api_path: str):
+        api_full_path = self.settings.api_endpoint + api_path
+        print('[API DELETE]', api_full_path)
+        try:
+            result = requests.delete(api_full_path, auth=HTTPBasicAuth(self.settings.api_key, ''))
+        except requests.HTTPError:
+            print('[HTTP DELETE] Error')
+            exit()
         else:
             result = json.loads(result.text)
         return result
@@ -56,9 +80,10 @@ class StreakConnection:
     class User:
         def __init__(self, streak_connection):
             self.streak_connection = streak_connection
+            self.displayName = 'not received yet'
 
         def __repr__(self):
-            return '<User Obj: %s >' % self.displayName
+            return '<User Obj: %s>' % self.displayName
 
         @property
         def me(self):
@@ -85,13 +110,47 @@ class StreakConnection:
     class Pipeline:
         def __init__(self, streak_connection):
             self.streak_connection = streak_connection
+            self.name = 'not received yet'
+
+        def __repr__(self):
+            return '<Pipeline Obj: %s>' % self.name
 
         @property
         def all(self):
-            return api_get(self, 'pipelines/')
+            pipelines_list = []
+            pipeline_dicts = api_get(self, 'pipelines/')
+            for pipeline_dict in pipeline_dicts:
+                pipelines_list.append(create_attrs(pipeline_dict, self.__class__(self.streak_connection)))
+            return pipelines_list
 
-        def get(self, pipeline_key):
-            return api_get(self, 'pipelines/' + pipeline_key)
+        def get(self, pipeline_key: str):
+            pipeline_dict = api_get(self, 'pipelines/' + pipeline_key)
+
+            if 'success' in pipeline_dict.keys():
+                raise Exception(pipeline_dict['error'])
+
+            new_pipeline_instance = create_attrs(pipeline_dict, self.__class__(self.streak_connection))
+            return new_pipeline_instance
+
+        def create(self, settings: dict):
+            api_path = 'pipelines/'
+            new_pipeline = self.streak_connection.put_api_data(api_path, settings)
+
+            if 'success' in new_pipeline.keys():
+                raise Exception(new_pipeline['error'])
+
+            print('[API PUT] New pipeline created with pipelineKey', new_pipeline['pipelineKey'])
+
+            return self.get(new_pipeline['pipelineKey'])
+
+        def delete(self, pipeline_key: str):
+            api_path = 'pipelines/' + pipeline_key
+            delete_result = self.streak_connection.delete_api_data(api_path)
+
+            if not delete_result['success']:
+                raise Exception('[HTTP DELETE] Failed to delete')
+            else:
+                print('[HTTP DELETE] Delete OK')
 
 if __name__ == '__main__':
     pass
